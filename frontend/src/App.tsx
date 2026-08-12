b11a234218c6cd8561128af952b86f41c0628267
sirironheart <127705750+Siriron@users.noreply.github.com>
2026-08-12 15:03:00 +0600

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,39 @@
+import { Routes, Route } from 'react-router-dom';
+import { NetworkProvider } from './hooks/useNetwork';
+import { GenLayerProvider } from './hooks/useGenLayer';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { LandingPage } from './pages/LandingPage';
+import { NewMilestonePage } from './pages/NewMilestonePage';
+import { VaultsListPage } from './pages/VaultsListPage';
+import { VaultDetailPage } from './pages/VaultDetailPage';
+import { DocsPage } from './pages/DocsPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
+export default function App() {
+  return (
+    <ErrorBoundary>
+      {/* NetworkProvider must wrap GenLayerProvider -- useGenLayer calls
+          useNetwork() internally to know which chain to read/write. */}
+      <NetworkProvider>
+        <GenLayerProvider>
+          <div className="flex min-h-screen flex-col">
+            <Header />
+            <main className="flex-1">
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/new" element={<NewMilestonePage />} />
+                <Route path="/vaults" element={<VaultsListPage />} />
+                <Route path="/vaults/:id" element={<VaultDetailPage />} />
+                <Route path="/docs" element={<DocsPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </GenLayerProvider>
+      </NetworkProvider>
+    </ErrorBoundary>
+  );
+}
